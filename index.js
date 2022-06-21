b2c00fe91b46e174c16f7b2cb2371f365bd0b29d
FadyEilia2001 <feilia@WGU.edu>
2022-06-20 19:47:20 -0500

    JS draft update
    Not working yet

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const numberBtn = document.querySelectorAll(".number")
-const display = document.getElementById('display')
+const mainDisplay = document.getElementById('display')
+const secondaryDisplay = document.getElementById('display-two')
 const clearBtn = document.getElementById('clear')
 const plusBtn = document.getElementById('plus')
-
-let numOne = parseInt(displayContent)
-console.log(numOne)
-let numTwo = 0
-
-let displayContent = "";
-
-
+const equalBtn = document.querySelector('.equal-sign')
+const operands = document.querySelectorAll('.operator')
+let currentNumber = []
+let displayNumbers = ""
+let initialNumber = 0
+let total = 0
 
 
 
-
-numberBtn.forEach((button) => {
-    
-    button.addEventListener('click', ()=>{
-        displayContent += button.textContent
+numberBtn.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        displayNumbers += button.textContent
         renderDisplay()
     })
 })
 
-clearBtn.addEventListener('click', cleardisplay)
-
-
-
-
-
-
-
-
-function renderDisplay () {
-    display.textContent = displayContent
-}
-
-function cleardisplay () {
-    displayContent = ""
+plusBtn.addEventListener('click', ()=>{
+    initialNumber = parseFloat(displayNumbers)
+    displayNumbers = "" //result
     renderDisplay()
+    currentNumber.push(initialNumber)
+    console.log(currentNumber)
+})
+
+
+
+
+function renderDisplay(){
+    mainDisplay.textContent = displayNumbers
 }
 
+function clearDisplay(){
+    displayNumbers = ""
+    mainDisplay.textContent = ""
+}
+
+//working on this
+function addNumbers(){
+    currentNumber.reduce((num, nextNum)=>{
+        return num + nextNum
+    })
+}
